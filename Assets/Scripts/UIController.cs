b04713dd293dbd90e76bbe134b009b77244a9c82
Kelemen Péter <peter.kelemen456@gmail.com>
2023-10-26 20:17:30 +0200

    Added base code for shovel options

--- Assets/Scripts/UIController.cs
+++ Assets/Scripts/UIController.cs
@@ -3,14 +3,16 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UIController : MonoBehaviour
 {
     public TextMeshProUGUI scoreText;
-
+    public List<Button> buttonList = new List<Button>();
     private int score = 0;
 
-
+    
+    
     void Start()
     {
         scoreText.SetText("Score: " + score);
@@ -21,6 +23,27 @@ public class UIController : MonoBehaviour
     {
     }
 
+
+    public void redClick()
+    {
+        Debug.Log("Red button clicked");
+    }
+
+    public void blueClick()
+    {
+        Debug.Log("Blue button clicked");
+    }
+
+    public void yellowClick()
+    {
+        Debug.Log("Yellow button clicked");
+    }
+
+    public void greenClick()
+    {
+        Debug.Log("Green button clicked");
+    }
+    
     public void receiveBlockValue(int value)
     {
         score += value;
