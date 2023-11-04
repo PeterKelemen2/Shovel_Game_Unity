8f4950e40f1fc16ee7dddfd7665e923b9eade3f9
Kelemen Péter <peter.kelemen456@gmail.com>
2023-11-04 15:36:38 +0100

    Changed rendering to URP

--- Assets/Scripts/Boundary_Scroller.cs
+++ Assets/Scripts/Boundary_Scroller.cs
@@ -4,24 +4,23 @@ using UnityEngine;
 
 public class Boundary_Scroller : MonoBehaviour
 {
-    [SerializeField] public Vector2 uvOffset = new Vector2(1f, 2f);
-    //[SerializeField] public Vector2 uvScale = new Vector2(1.0f, 1.0f);
-
     private Renderer rend;
-    private Material material;
-    private float offsetSpeed = 1f;
+    private Material boundaryMaterial;
     private Vector2 offset;
+    private Vector2 uvScale = new(1f, 20f);
 
     void Start()
     {
-        material = GetComponentInChildren<Renderer>().material;
-        offset = material.GetTextureOffset("_MainTex");
+        rend = GetComponentInChildren<Renderer>();
+        boundaryMaterial = rend.material;
+        boundaryMaterial.mainTextureScale = uvScale;
+        offset.y = transform.position.y;
+        boundaryMaterial.mainTextureOffset = offset;
     }
 
     void Update()
     {
-        float newYOffset = transform.position.y * offsetSpeed;
-        offset.y = newYOffset;
-        material.SetTextureOffset("_MainTex", offset);
+        offset.y = transform.position.y;
+        boundaryMaterial.mainTextureOffset = offset;
     }
 }
